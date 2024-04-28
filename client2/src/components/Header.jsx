54c8e2683dd57772ceafc020e09d146fba291b78
AbdurRehman1312 <abdurrehmansaifi784@gmail.com>
2024-04-28 16:02:16 +0500

    Profile and header UI

--- client2/src/components/Header.jsx
+++ client2/src/components/Header.jsx
@@ -5,8 +5,8 @@ export default function Header() {
   const user = useSelector((state) => state.user.user.user);
   console.log(user);
   return (
-    <div className="bg-slate-200 ">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+    <div className="bg-[#234795] text-white ">
+      <div className="flex justify-between items-center h-[10vh] p-3">
         <h1 className=" font-bold">Auth App</h1>
         <ul className="flex gap-4">
           <li>
--- client2/src/pages/Home.jsx
+++ client2/src/pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
   };
 
   return (
-    <div>
+    <div className="p-3">
       <h1 className="text-2xl font-bold mb-4">Choose Your Character</h1>
 
       <div className="grid grid-cols-2 gap-4">
