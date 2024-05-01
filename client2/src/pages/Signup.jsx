adb1b93652e3c5183364929b2ed9e79b82ecef2e
aliabbas827 <muhammadaliabbas827@gmail.com>
2024-05-01 13:16:07 +0500

    Signup UI

--- client2/src/pages/Signup.jsx
+++ client2/src/pages/Signup.jsx
@@ -42,53 +42,58 @@ export default function Signup() {
     }
   };
   return (
-    <div className="max-w-lg mx-auto">
-      <h1 className="text-3xl text-center font-semibold my-7">Signup</h1>
-      <form
-        onSubmit={handleSubmit}
-        className="flex w-full flex-col gap-4 items-center"
-      >
-        <input
-          type="text"
-          placeholder="Username"
-          id="username"
-          className="border w-full bg-slate-100 border-gray-400 p-3 rounded-lg my-2"
-          onChange={handleChange}
-        />
-        <input
-          type="email"
-          id="email"
-          placeholder="Email"
-          className="border w-full bg-slate-100 border-gray-400 p-3 rounded-lg my-2"
-          onChange={handleChange}
-        />
-        <input
-          type="password"
-          id="password"
-          placeholder="Password"
-          className="border w-full bg-slate-100 border-gray-400 p-3 rounded-lg my-2"
-          onChange={handleChange}
-        />
-        <button className="bg-slate-700 w-full text-white p-3 rounded-lg disabled:opacity-80 uppercase hover:opacity-95 my-2">
-          {loading ? "Loading" : "Signup"}
-        </button>
-        <Oauth />
+    <>
+      <section className="bg-[#234795] h-[90vh] flex justify-center items-center">
+        <div className="mx-auto lg:w-[30%] md:w-[70%] w-[95%] bg-[#1c140f] backdrop-blur-lg backdrop-filter rounded-2xl p-6 shadow-2xl">
+          <h1 className="text-3xl font-semibold text-gray-200 text-center my-7">Signup</h1>
+          <form
+            onSubmit={handleSubmit}
+            className="flex flex-col gap-4"
+          >
+            <input
+              type="text"
+              placeholder="Username"
+              id="username"
+              className="w-full p-3 rounded-lg my-2"
+              onChange={handleChange}
+            />
+            <input
+              type="email"
+              id="email"
+              placeholder="Email"
+              className="w-full p-3 rounded-lg my-2"
+              onChange={handleChange}
+            />
+            <input
+              type="password"
+              id="password"
+              placeholder="Password"
+              className="w-full p-3 rounded-lg my-2"
+              onChange={handleChange}
+            />
+            <button className="btn text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80 shadow-md my-2 w-full">
+              {loading ? "Loading" : "Signup"}
+            </button>
 
-        {error && (
-          <div className="bg-red-100 text-red-700 p-3 rounded-lg my-2">
-            {error ? error.message : "Something went wrong"}
+            {error && (
+              <div className="bg-red-100 text-red-700 p-3 rounded-lg my-2">
+                {error ? error.message : "Something went wrong"}
+              </div>
+            )}
+          </form>
+          <div className=" ">
+            <Link
+              to="/signin"
+              className=" flex  gap-2 mt-5 items-center text-center text-white"
+            >
+              <p className=" p-0 m-0">Have an account?</p>
+              <span className="p-0 m-0 text-orange-400">Sign in</span>
+            </Link>
           </div>
-        )}
-      </form>
-      <div className=" ">
-        <Link
-          to="/signin"
-          className=" flex  gap-2 mt-5 items-center text-center "
-        >
-          <p className=" p-0 m-0">Have an account?</p>
-          <span className="text-blue-500 p-0 m-0">Sign in</span>
-        </Link>
-      </div>
-    </div>
+        </div>
+      </section>
+
+    </>
+
   );
 }
