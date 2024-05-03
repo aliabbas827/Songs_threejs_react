0b8e0e33724316efd05758e8d13be97249b0cc19
AbdurRehman1312 <abdurrehmansaifi784@gmail.com>
2024-05-03 18:29:49 +0500

    Character selection model rendering

--- client2/src/pages/SongSelection.jsx
+++ client2/src/pages/SongSelection.jsx
@@ -1,7 +1,84 @@
-import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import React, { useState, Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
 import { AudioRecorder } from "react-audio-voice-recorder";
 import { hardcoded_songs } from "./songs";
+import { useGLTF, OrbitControls, Environment } from "@react-three/drei";
+import { useSelector } from "react-redux";
+
+function Model(props) {
+  const { nodes, materials } = useGLTF("/monster/monster.gltf");
+  return (
+    <group {...props} dispose={null}>
+      <group
+        position={[0, 1.307, 0]}
+        rotation={[Math.PI / 2, 0, 0]}
+        scale={1.171}
+      >
+        <mesh geometry={nodes.Body.geometry} material={materials.lambert2} />
+        <mesh
+          geometry={nodes.Body_fur.geometry}
+          material={materials.Material}
+        />
+        <mesh
+          geometry={nodes.eyes.geometry}
+          material={materials.lambert2}
+          position={[0, 0, 1.113]}
+          scale={0.038}
+        />
+        <mesh
+          geometry={nodes.Teeth.geometry}
+          material={materials.lambert2}
+          position={[0, 0, 1.113]}
+          scale={0.038}
+        />
+        <mesh
+          geometry={nodes.Top_Hair.geometry}
+          material={materials.lambert2}
+          position={[0, 0, 1.113]}
+          scale={0.038}
+        />
+      </group>
+    </group>
+  );
+}
+function Model2(props) {
+  const { nodes, materials } = useGLTF("/professor/professor.gltf");
+  return (
+    <group {...props} dispose={null}>
+      <group rotation={[Math.PI / 2, 0, 0]} scale={0.01}>
+        <mesh geometry={nodes.beard_geo.geometry} material={materials.Hair} />
+        <mesh geometry={nodes.Body_geo.geometry} material={materials.Body} />
+        <mesh geometry={nodes.Bowtie_geo.geometry} material={materials.Coat} />
+        <mesh geometry={nodes.Coat_geo.geometry} material={materials.Coat} />
+        <mesh geometry={nodes.Eye_geo.geometry} material={materials.Eyes} />
+        <mesh geometry={nodes.Eyebrow_geo.geometry} material={materials.Hair} />
+        <mesh geometry={nodes.Hair_geo.geometry} material={materials.Hair} />
+        <mesh
+          geometry={nodes.Pant_geo.geometry}
+          material={materials.PAntandshirt}
+        />
+        <mesh
+          geometry={nodes.Shirt_geo.geometry}
+          material={materials.PAntandshirt}
+        />
+        <mesh geometry={nodes.Shoes_geo.geometry} material={materials.Shoe} />
+        <mesh
+          geometry={nodes.Specs_frame_geo.geometry}
+          material={materials.Specs}
+        />
+        <mesh
+          geometry={nodes.Specs_glass_geo.geometry}
+          material={materials.Specs}
+        />
+        <mesh geometry={nodes.Teeth_geo.geometry} material={materials.Teeth} />
+        <mesh
+          geometry={nodes.Waistcoat_geo.geometry}
+          material={materials.Coat}
+        />
+      </group>
+    </group>
+  );
+}
 
 const Recorder = ({ recordedAudio, setRecordedAudio }) => {
   const [audioUrl, setAudioUrl] = useState(null);
@@ -94,7 +171,7 @@ const SongSelection = () => {
   // };
 
   return (
-    <div className="p-4">
+    <div className="p-4 bg-[#234795] bg-opacity-90">
       <h1 className="text-2xl font-bold mb-4">
         Thanks for choosing a character
       </h1>
@@ -108,9 +185,77 @@ const SongSelection = () => {
       </div>
       <div className="mb-4">
         {user?.character === "Professor" ? (
-          <h1>Professor</h1>
+          <div
+          className="bg-[#1c140f] rounded-2xl h-[300px]"
+        >
+          <Canvas
+            style={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+            }}
+            className="responsive-canvas"
+          >
+            <Suspense fallback={null}>
+              <directionalLight />
+              <spotLight
+                intensity={0.9}
+                angle={0.1}
+                penumbra={1}
+                position={[10, 15, 10]}
+                castShadow
+              />
+              <Model2 scale={[3.5, 3.5, 3.5]} position={[0, -2.4, 0]} />
+              <OrbitControls
+                enablePan={true}
+                enableZoom={false}
+                enableRotate={true}
+                minPolarAngle={Math.PI / 2.1}
+                maxPolarAngle={Math.PI / 2.1}
+              />
+              <Environment preset="sunset" />
+            </Suspense>
+          </Canvas>
+        </div>
         ) : (
-          <h1>Student</h1>
+          <div className="flex justify-evenly items-center flex-wrap gap-10">
+        <div
+          className= "bg-[#1c140f] rounded-2xl h-[300px]"
+        >
+          <Canvas
+            style={{
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              justifyContent: "center",
+            }}
+            className="responsive-canvas"
+          >
+            <Suspense fallback={null}>
+              {/* <directionalLight /> */}
+              <spotLight
+                intensity={0.9}
+                angle={0.1}
+                penumbra={1}
+                position={[10, 15, 10]}
+                castShadow
+              />
+              <Model scale={[1.5, 1.5, 1.5]} position={[0, -2, 0]} />{" "}
+              {/* Adjust scale as needed */}
+              <OrbitControls
+                enablePan={true}
+                enableZoom={false}
+                enableRotate={true}
+                minPolarAngle={Math.PI / 2.1}
+                maxPolarAngle={Math.PI / 2.1}
+              />
+              <Environment preset="sunset" />
+            </Suspense>
+          </Canvas>
+        </div>
+      </div>
         )}
       </div>
 
